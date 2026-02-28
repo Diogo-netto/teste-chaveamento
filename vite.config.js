3c1bf1cf764b4d2f343e227f2f7585466330bdf0
Diogo Netto <diogonetto.ti@gmail.com>
2026-02-27 22:36:54 -0300

    Criada tela de Ranking Global e função de Backup aprimorada

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // O nome exato do seu repositório no GitHub entre barras
+  base: '/teste-chaveamento/', 
+  
   server: {
     port: 3001,
-  },
+  }
 })
